08185372a7ec8e9e1c73c1fe0300efc831b27c1b
gajendraverma <gajendraverma353@gmail.com>
2024-09-20 00:24:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from"./Components/Navbar";
+import Sidenavbar from "./Components/Sidenavbar";
+function App(){
+    return( 
+    <div>
+    <Navbar />
+    <Sidenavbar />
+    </div>
+    )
+}
+
+export default App;
---
+++ src/Components/Sidenavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidenavbar() {
+  return (
+    <div>
+        <h1>oooyyyy</h1>
+    </div>
+  )
+}
+
+export default Sidenavbar;
